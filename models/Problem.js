4ba18a915d6aea8413654963bdffe61be13718bc
tpdud406 <tpdud406@naver.com>
2022-11-27 01:02:31 +0900

    feat: DB연결, 스키마 생성

---
+++ models/Problem.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const ProblemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tests: [
+    {
+      description: { type: String, required: true },
+      input: { type: mongoose.Schema.Types.Mixed, required: true },
+      output: { type: mongoose.Schema.Types.Mixed, required: true },
+    },
+  ],
+  averageRuntimes: [Number],
+});
+
+module.exports = mongoose.model("Problem", ProblemSchema);
