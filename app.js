5a5ca0b4159a93cfdeef8d5deb782543090d9a1c
tpdud406 <tpdud406@naver.com>
2022-11-28 02:28:59 +0900

    feat: cors설정, 로그인 구현

--- app.js
+++ app.js
@@ -3,21 +3,27 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 const db = require("./config/connectDB");
+const corsOptions = {
+  origin: [process.env.CLIENT_HOST],
+  credentials: true,
+};
 
-const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
 const usersRouter = require("./routes/users");
 
 const app = express();
 db();
 
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/auth", authRouter);
 app.use("/users", usersRouter);
 
 app.use(function (req, res, next) {
@@ -29,7 +35,7 @@ app.use((err, req, res, next) => {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  res.render("error");
+  res.send("error");
 });
 
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { login } = require("./controllers/auth.controller");
 
-router.get("/", function (req, res, next) {
-  res.send("Project init");
-});
+router.post("/login", login);
 
 module.exports = router;
