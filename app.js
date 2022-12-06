e4a3214ac48afadb244f054436b8dd0985d95268
tpdud406 <tpdud406@naver.com>
2022-12-06 20:41:52 +0900

    feat: 배포확인

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/auth", authRouter);
 app.use("/users", usersRouter);
+app.use("/", (req, res, next) => res.send("dd"));
 
 app.use(function (req, res, next) {
   next(createError(404));
